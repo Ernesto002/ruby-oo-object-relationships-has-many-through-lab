14dafef191c8e19d890595430330960d3b649de7
ernesto <ernesto002@icloud.com>
2020-12-04 13:27:50 -0800

    commit tree 1d343dade6a50168f2310926ffc594a4695c99dd

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,28 @@
+class Genre
 
+    attr_reader :name, :genre
+    attr_accessor :song
+
+    GENRE = []
+  
+    def initialize(name)
+      @name = name
+      GENRE << self
+    end
+  
+    def songs
+      Song.all.select do |song|
+        song.genre == self
+      end
+    end
+  
+    def artists
+      Song.all.map do |song|
+        song.artist
+      end
+    end
+
+    def self.all 
+        GENRE
+    end
+end
